96363d37f79c6e280eef22bf8ae1e77686de03a3
liam <liam@Liams-Air.local>
2023-03-27 09:16:17 +0800

    feat: 初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Pixi/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Pixi from "@/components/Pixi2.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pixi,
   }
 }
 </script>
---
+++ src/components/Pixi2.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import * as PIXI from "pixi.js";
+export default {
+  async mounted() {
+    // renderer 是渲染器；一切故事从这开始
+    let app = new PIXI.Application();
+    document.body.appendChild(app.view);
+    const texture = PIXI.Texture.from('logo.png');
+    const sprite = new PIXI.Sprite(texture);
+    app.stage.addChild(sprite);
+    // 参照官网查看 sprite实例的属性；
+    console.log(sprite)
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
