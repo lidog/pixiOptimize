9116f5c290e53dbc38484f44d345f0cdc05fabac
liam <liam@Liams-Air.local>
2023-05-04 17:22:42 +0800

    feat: 拖拽标牌牵引线

--- src/track/trackBusiness.js
+++ src/track/trackBusiness.js
@@ -52,10 +52,41 @@ function DragLabel(trackDom, viewport) {
         const point = trackLabel.toLocal(moveEvent.global);
         trackLabel.position.x = trackLabel.position.x + point.x;
         trackLabel.position.y = trackLabel.position.y + point.y;
+        redrawLine(trackLabel, trackLine);
       }
     })
     trackLabel.on("pointerup", () => {
       isDragEnd = true;
     });
   }, trackLabel)
+}
+
+function redrawLine(label, line) {
+  let rect_pos = label.position;
+  let line_pos = line.position;
+  // Math.atan2 计算二维坐标系中任意一个点(x,y)和原点(0,0)的连线与x轴正半轴的夹角角度
+  let criticalTheta = Math.atan2(label.height / 2, label.width / 2);
+  //Math.sqrt 求平方根
+  let distance = Math.sqrt(
+    // Math.pow(a, b) 计算a的b次方
+    Math.pow(rect_pos.x - line_pos.x, 2) +
+    Math.pow(rect_pos.y - line_pos.y, 2)
+  );
+  let dd = 0
+  let theta = Math.atan2(rect_pos.y - line_pos.y, rect_pos.x - line_pos.x);
+  if (
+    Math.abs(theta) < criticalTheta ||
+    Math.abs(theta) > Math.PI - criticalTheta
+  ) {
+    dd = label.width / 2 / Math.abs(Math.cos(Math.abs(theta)));
+  } else {
+    dd = label.height / 2 / Math.abs(Math.sin(Math.abs(theta)));
+  }
+  if (distance - dd > 0) {
+    line.rotation = theta;
+    line.width = distance - dd;
+  } else {
+    line.rotation = 0;
+    line.width = 0;
+  }
 }
